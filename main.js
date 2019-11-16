7ae10612f0d09ac1338d7cc67de3cce051158be1
Paola <pao.franzini@gmail.com>
2019-11-16 18:21:03 +0100

    Finito!

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
 let myApiKey = '0839ff69e6bf3e71fbc91550342cb460';
 
-document.querySelector('#search-field').focus();
-document.querySelector('#search-button').addEventListener('click', searchTerm);
-
 let daysDisplayed = [];
 let weatherOnDate = [];
 
+let body = document.querySelector('body');
+let mainTitle = document.querySelector('#main-title');
+let introText = document.querySelector('#intro-text');
+let searchField = document.querySelector('#search-field');
+let searchButton = document.querySelector('#search-button');
 let searchContainer = document.querySelector('#search-container');
-let cityName = document.querySelector('#city-name').textContent;
-let country = document.querySelector('#country').textContent;
+let cityName = document.querySelector('#city-name');
+let country = document.querySelector('#country');
 let daySelection = document.querySelector('#day-selection');
 let forecastContainer = document.querySelector('#forecast-container');
+
+searchField.focus();
+searchButton.addEventListener('click', searchTerm);
+
+searchField.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    searchButton.click();
+    searchField.blur();
+  }
+});
 
 function searchTerm() {
   let cityToSearch = document.querySelector('#search-field').value;
   if (cityToSearch) {
     searchWeather(cityToSearch);
   }
+  document.querySelector('#search-field').value = '';
 }
 
 function searchWeather(searchCity) {
+  if (introText.childNodes[1] === mainTitle) {
+    introText.removeChild(mainTitle);
+  }
+
   let url =
     'https://api.openweathermap.org/data/2.5/forecast?q=' +
     searchCity +
-    '&units=metric&APPID=' +
+    '&lang=it&units=metric&APPID=' +
     myApiKey;
   console.log(url);
   fetch(url)
@@ -34,19 +52,33 @@ function searchWeather(searchCity) {
       initializeAll(result);
     })
     .catch(() => {
-      let searchError = document.createElement('p');
-      searchError.setAttribute('id', 'search-error');
-      searchContainer.appendChild(searchError);
-      let myError = document.createTextNode(
-        `Result not found for "${searchCity}". Please check the spelling.`
+      body.classList.remove('weather-displayed');
+      searchButton.blur();
+
+      let searchErrorContainer = document.createElement('div');
+      searchErrorContainer.setAttribute('id', 'search-error');
+      searchContainer.appendChild(searchErrorContainer);
+
+      let notFound = document.createElement('p');
+      searchErrorContainer.appendChild(notFound);
+      let myNotFound = document.createTextNode(
+        `Non ho trovato la città "${searchCity}".`
       );
-      searchError.appendChild(myError);
+      notFound.appendChild(myNotFound);
+
+      let checkSpelling = document.createElement('p');
+      searchErrorContainer.appendChild(checkSpelling);
+      let myCheck = document.createTextNode(
+        'Per favore, controlla di aver scritto correttamente e fai una nuova ricerca'
+      );
+      checkSpelling.appendChild(myCheck);
     });
 }
 
 function initializeAll(resultFromServer) {
   console.log(resultFromServer);
 
+  body.classList.add('weather-displayed');
   weatherOnDate = [];
 
   let searchError = document.querySelector('#search-error');
@@ -56,6 +88,8 @@ function initializeAll(resultFromServer) {
 
   daySelection.innerHTML = '';
   forecastContainer.innerHTML = '';
+  cityName.textContent = '';
+  country.textContent = '';
 
   displayInfoCity(resultFromServer.city);
   displayDays(resultFromServer.list);
@@ -83,7 +117,7 @@ function displayDays(list) {
   }
 
   for (i = 0; i < daysDisplayed.length; i++) {
-    let newDay = document.createElement('div');
+    let newDay = document.createElement('a');
     newDay.setAttribute('class', 'days');
 
     let numberDate = document.createElement('p');
@@ -111,19 +145,19 @@ function dateToWeekday(stringDate) {
 
   switch (myDate.getDay()) {
     case 0:
-      return 'Sun';
+      return 'Dom';
     case 1:
-      return 'Mon';
+      return 'Lun';
     case 2:
-      return 'Tue';
+      return 'Mar';
     case 3:
-      return 'Wed';
+      return 'Mer';
     case 4:
-      return 'Thu';
+      return 'Gio';
     case 5:
-      return 'Fri';
+      return 'Ven';
     case 6:
-      return 'Sat';
+      return 'Sab';
   }
 }
 
@@ -159,35 +193,88 @@ function initializeToday() {
   let listDays = document.querySelectorAll('.days');
   listDays[0].classList.add('active');
 
-  let hourRow = document.createElement('div');
-  hourRow.setAttribute('class', 'hour-row');
-  forecastContainer.appendChild(hourRow);
+  let titleRow = document.createElement('div');
+  titleRow.setAttribute('class', 'title-row');
+  forecastContainer.appendChild(titleRow);
+
+  let timeTitle = document.createElement('p');
+  timeTitle.setAttribute('class', 'title-detail time-title');
+  titleRow.appendChild(timeTitle);
+  let myTitleTime = document.createTextNode('');
+  timeTitle.appendChild(myTitleTime);
+
+  let descTitle = document.createElement('p');
+  descTitle.setAttribute('class', 'title-detail desc-title');
+  titleRow.appendChild(descTitle);
+  let myTitleDesc = document.createTextNode('');
+  descTitle.appendChild(myTitleDesc);
+
+  let tempTitle = document.createElement('p');
+  tempTitle.setAttribute('class', 'title-detail temp-title');
+  titleRow.appendChild(tempTitle);
+  let myTitleTemp = document.createTextNode('°C');
+  tempTitle.appendChild(myTitleTemp);
+
+  let ventoTitle = document.createElement('div');
+  ventoTitle.setAttribute('class', 'title-detail vento-title');
+  titleRow.appendChild(ventoTitle);
+
+  let ventoWord = document.createElement('p');
+  ventoWord.setAttribute('class', 'vento-sub vento-word');
+  ventoTitle.appendChild(ventoWord);
+  let myWordVento = document.createTextNode('Vento');
+  ventoWord.appendChild(myWordVento);
+
+  let ventoUnit = document.createElement('p');
+  ventoUnit.setAttribute('class', 'vento-sub vento-unit');
+  ventoTitle.appendChild(ventoUnit);
+  let myUnitVento = document.createTextNode('m/s');
+  ventoUnit.appendChild(myUnitVento);
+
+  let umidTitle = document.createElement('p');
+  umidTitle.setAttribute('class', 'title-detail umid-title');
+  titleRow.appendChild(umidTitle);
+  let myTitleUmid = document.createTextNode('Umid.');
+  umidTitle.appendChild(myTitleUmid);
 
   for (i = 0; i < weatherOnDate[0].length; i++) {
+    let hourRow = document.createElement('div');
+    hourRow.setAttribute('class', 'hour-row');
+    forecastContainer.appendChild(hourRow);
+
     let time = document.createElement('p');
-    time.setAttribute('class', 'time');
+    time.setAttribute('class', 'weather-detail time');
     hourRow.appendChild(time);
     let myTime = document.createTextNode(
       weatherOnDate[0][i].dt_txt.slice(11, 16)
     );
     time.appendChild(myTime);
 
-    let icon = document.createElement('p');
+    let descriptionContainer = document.createElement('div');
+    descriptionContainer.setAttribute(
+      'class',
+      'weather-detail description-container'
+    );
+    hourRow.appendChild(descriptionContainer);
+
+    let icon = document.createElement('img');
     icon.setAttribute('class', 'icon');
-    hourRow.appendChild(icon);
-    let myIcon = document.createTextNode(weatherOnDate[0][i].weather[0].icon);
-    icon.appendChild(myIcon);
+    icon.src =
+      'http://openweathermap.org/img/wn/' +
+      weatherOnDate[0][i].weather[0].icon +
+      '@2x.png';
+    descriptionContainer.appendChild(icon);
 
     let description = document.createElement('p');
     description.setAttribute('class', 'description');
-    hourRow.appendChild(description);
+    descriptionContainer.appendChild(description);
     let myDescription = document.createTextNode(
       weatherOnDate[0][i].weather[0].description
     );
     description.appendChild(myDescription);
 
     let temperature = document.createElement('p');
-    temperature.setAttribute('class', 'temperature');
+    temperature.setAttribute('class', 'weather-detail temperature');
     hourRow.appendChild(temperature);
     let myTemperature = document.createTextNode(
       Number(weatherOnDate[0][i].main.temp).toFixed(1) + '°'
@@ -195,15 +282,13 @@ function initializeToday() {
     temperature.appendChild(myTemperature);
 
     let wind = document.createElement('p');
-    wind.setAttribute('class', 'wind');
+    wind.setAttribute('class', 'weather-detail wind');
     hourRow.appendChild(wind);
-    let myWind = document.createTextNode(
-      weatherOnDate[0][i].wind.speed + 'm/s'
-    );
+    let myWind = document.createTextNode(weatherOnDate[0][i].wind.speed);
     wind.appendChild(myWind);
 
     let humidity = document.createElement('p');
-    humidity.setAttribute('class', 'humidity');
+    humidity.setAttribute('class', 'weather-detail humidity');
     hourRow.appendChild(humidity);
     let myHumidity = document.createTextNode(
       weatherOnDate[0][i].main.humidity + '%'
@@ -233,42 +318,94 @@ function updateInfoDate() {
   listDays[index].classList.add('active');
 
   let childrenForecastContainer = forecastContainer.childNodes;
+  let numChild = childrenForecastContainer.length;
 
-  for (i = 0; i < childrenForecastContainer.length; i++) {
-    forecastContainer.removeChild(childrenForecastContainer[i]);
+  for (i = 0; i < numChild; i++) {
+    forecastContainer.removeChild(childrenForecastContainer[0]);
   }
 
-  let hourRow = document.createElement('div');
-  hourRow.setAttribute('class', 'hour-row');
-  forecastContainer.appendChild(hourRow);
+  let titleRow = document.createElement('div');
+  titleRow.setAttribute('class', 'title-row');
+  forecastContainer.appendChild(titleRow);
+
+  let timeTitle = document.createElement('p');
+  timeTitle.setAttribute('class', 'title-detail time-title');
+  titleRow.appendChild(timeTitle);
+  let myTitleTime = document.createTextNode('');
+  timeTitle.appendChild(myTitleTime);
+
+  let descTitle = document.createElement('p');
+  descTitle.setAttribute('class', 'title-detail desc-title');
+  titleRow.appendChild(descTitle);
+  let myTitleDesc = document.createTextNode('');
+  descTitle.appendChild(myTitleDesc);
+
+  let tempTitle = document.createElement('p');
+  tempTitle.setAttribute('class', 'title-detail temp-title');
+  titleRow.appendChild(tempTitle);
+  let myTitleTemp = document.createTextNode('°C');
+  tempTitle.appendChild(myTitleTemp);
+
+  let ventoTitle = document.createElement('div');
+  ventoTitle.setAttribute('class', 'title-detail vento-title');
+  titleRow.appendChild(ventoTitle);
+
+  let ventoWord = document.createElement('p');
+  ventoWord.setAttribute('class', 'vento-sub vento-word');
+  ventoTitle.appendChild(ventoWord);
+  let myWordVento = document.createTextNode('Vento');
+  ventoWord.appendChild(myWordVento);
+
+  let ventoUnit = document.createElement('p');
+  ventoUnit.setAttribute('class', 'vento-sub vento-unit');
+  ventoTitle.appendChild(ventoUnit);
+  let myUnitVento = document.createTextNode('m/s');
+  ventoUnit.appendChild(myUnitVento);
+
+  let umidTitle = document.createElement('p');
+  umidTitle.setAttribute('class', 'title-detail umid-title');
+  titleRow.appendChild(umidTitle);
+  let myTitleUmid = document.createTextNode('Umid.');
+  umidTitle.appendChild(myTitleUmid);
 
   for (i = 0; i < weatherOnDate[index].length; i++) {
+    let hourRow = document.createElement('div');
+    hourRow.setAttribute('class', 'hour-row');
+    forecastContainer.appendChild(hourRow);
+
     let time = document.createElement('p');
-    time.setAttribute('class', 'time');
+    time.setAttribute('class', 'weather-detail time');
     hourRow.appendChild(time);
     let myTime = document.createTextNode(
       weatherOnDate[index][i].dt_txt.slice(11, 16)
     );
     time.appendChild(myTime);
 
-    let icon = document.createElement('p');
-    icon.setAttribute('class', 'icon');
-    hourRow.appendChild(icon);
-    let myIcon = document.createTextNode(
-      weatherOnDate[index][i].weather[0].icon
+    let descriptionContainer = document.createElement('div');
+    descriptionContainer.setAttribute(
+      'class',
+      'weather-detail description-container'
     );
-    icon.appendChild(myIcon);
+    hourRow.appendChild(descriptionContainer);
+
+    let icon = document.createElement('img');
+    icon.setAttribute('class', 'icon');
+    icon.src =
+      'http://openweathermap.org/img/wn/' +
+      weatherOnDate[index][i].weather[0].icon +
+      '@2x.png';
+    descriptionContainer.appendChild(icon);
 
     let description = document.createElement('p');
     description.setAttribute('class', 'description');
-    hourRow.appendChild(description);
+    descriptionContainer.appendChild(description);
     let myDescription = document.createTextNode(
       weatherOnDate[index][i].weather[0].description
     );
     description.appendChild(myDescription);
 
     let temperature = document.createElement('p');
-    temperature.setAttribute('class', 'temperature');
+    temperature.setAttribute('class', 'weather-detail temperature');
     hourRow.appendChild(temperature);
     let myTemperature = document.createTextNode(
       Number(weatherOnDate[index][i].main.temp).toFixed(1) + '°'
@@ -276,15 +413,13 @@ function updateInfoDate() {
     temperature.appendChild(myTemperature);
 
     let wind = document.createElement('p');
-    wind.setAttribute('class', 'wind');
+    wind.setAttribute('class', 'weather-detail wind');
     hourRow.appendChild(wind);
-    let myWind = document.createTextNode(
-      weatherOnDate[index][i].wind.speed + 'm/s'
-    );
+    let myWind = document.createTextNode(weatherOnDate[index][i].wind.speed);
     wind.appendChild(myWind);
 
     let humidity = document.createElement('p');
-    humidity.setAttribute('class', 'humidity');
+    humidity.setAttribute('class', 'weather-detail humidity');
     hourRow.appendChild(humidity);
     let myHumidity = document.createTextNode(
       weatherOnDate[index][i].main.humidity + '%'
